8e6f78bf43ae3e661ad8fb72705f297efc598a93
ArdasAston <mauro.a.papagno@gmail.com>
2022-04-09 16:33:08 +0200

    added paddle class

---
+++ Arkanoid/Paddle.cpp
@@ -0,0 +1,37 @@
+#include "pch.h"
+#include "Paddle.h"
+
+Paddle::Paddle() {}
+
+Paddle::Paddle(int w, int h)
+{
+	screenSize = Vector2(w, h);
+	position = Vector2((w / 2) - (SPRITEWIDTH / 1.5f), h - 20);
+}
+
+Vector2 Paddle::GetPosition()
+{
+	return position;
+}
+
+void Paddle::SetPosition(Vector2 pos)
+{
+	position = pos;
+}
+
+void Paddle::MovePaddle(bool dir)
+{
+	if (dir && position.x + BORDEROFFSET + SPRITEWIDTH / 1.5f < screenSize.x)
+	{
+		position.x += PADDLESPEED;
+	}
+	else if (!dir && position.x > BORDEROFFSET - SPRITEWIDTH)
+	{
+		position.x -= PADDLESPEED;
+	}
+}
+
+Vector2 Paddle::GetScreenSize()
+{
+	return screenSize;
+}
---
+++ Arkanoid/Paddle.h
@@ -0,0 +1,22 @@
+#pragma once
+#define SPRITEWIDTH 64
+#define BORDEROFFSET 78
+#define PADDLESPEED 1.5f
+
+using namespace DirectX::SimpleMath;
+using namespace std;
+
+class Paddle
+{
+public:
+	Paddle();
+	Paddle(int, int);
+
+	Vector2 GetPosition();
+	void SetPosition(Vector2);
+	void MovePaddle(bool);
+	Vector2 GetScreenSize();
+private:
+	Vector2 position;
+	Vector2 screenSize;
+};
